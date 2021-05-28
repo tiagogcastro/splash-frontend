9c44cdc33d8ede9398e1cd1367937b194fdf8e13
Mateus Queirós <mateuscqueiros1@gmail.com>
2021-05-28 15:56:04 -0300

    refactor: Estrutura de pastas, variáveis e fontes (fontes ainda não funcionam)

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/pages/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+        <h1>Olá mundo</h1>
+    </div>
+  )
+}
