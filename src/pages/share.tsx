6de01e40613436ee39d6011b57405934af33e7b7
joaorceschini <jrvceschini@gmail.com>
2021-06-01 20:04:58 -0300

    Página de compartilhamento iniciada.

---
+++ src/pages/share.tsx
@@ -0,0 +1,33 @@
+import Header from '../components/Header';
+import Link from 'next/link'
+
+import styles from '../styles/pages/share.module.scss'
+import { useRouter } from 'next/router';
+
+export default function Share() {
+
+  const router = useRouter()
+  const query = router.query;
+
+  const cod = query.cod as string;
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Header text={"Nome da loja"} returnPage="/home" />
+        <div className={styles.content}>
+          <div className={styles.img}>
+            <img src="/public/qrcode.png" alt="" />
+          </div>
+
+          <div className={styles.text}>
+            <h2>{cod}</h2>
+            <Link href="/">
+              <a>lavimco.com</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
