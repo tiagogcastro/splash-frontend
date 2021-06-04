d8136279f61cb094b3aab048d2c225e6d439bd7d
Vitor Klein <vitorhklein2@gmail.com>
2021-06-04 15:25:29 -0300

    feat: add loginEmail and loginNumber to aplication

--- src/pages/loginEmail.tsx
+++ src/pages/loginEmail.tsx
@@ -1,19 +1,21 @@
 import Button from '@components/Button';
 
-import styles from '@styles/pages/login.module.scss';
+import styles from '@styles/pages/loginEmail.module.scss';
 
-export default function Login() {
+export default function LoginEmail() {
   return (
     <>
       <div className={styles.container}>
           <div className={styles.avatar}></div>
 
           <span>Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit ut aliquam</span>
-
-          <input type="text" placeholder="Insira seu código de patrocínio"/>
-
-          <Button>Continue com WhatsApp</Button>
-          <Button>Use e-mail ou nome de usuario</Button>
+          
+          <div className={styles.inputs}>
+            <input type="text" placeholder="Digite seu E-mail"/>
+            <input type="text" placeholder="Digite sua Senha"/>
+          </div>
+          
+          <Button>Entrar</Button>
       </div>
 
       <div className={styles.links}>
