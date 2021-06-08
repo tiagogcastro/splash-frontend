6839d0919c272fd80bd376b8c8f90146ef19bdc6
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-08 10:15:41 -0300

    Feat: Add verification if url prop is filled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { ButtonHTMLAttributes } from 'react'
 
 import styles from './styles.module.scss';
@@ -7,14 +8,25 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button: React.FC<ButtonProps> = ({ children, url = '', ...rest }) => {
-  return (
-    <a href={url} className={styles.container}>
-      <button {...rest} className={styles.button}>
-        {children}
-      </button>
-    </a>
-    
-  );
+  if (url) {
+    return (
+      <Link href={url}>
+        <a className={styles.container}>
+          <button {...rest} className={styles.button}>
+            {children}
+          </button>
+        </a>
+      </Link>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <button {...rest} className={styles.button}>
+          {children}
+        </button>
+      </div>
+    );
+  }
 };
 
 export default Button;
