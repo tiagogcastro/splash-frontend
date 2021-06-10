9ff41b64c9067e4313b579c4ffd00757183ecf94
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-09 22:35:54 -0300

    Refactor: Delete some unused lines

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -6,12 +6,7 @@ import Error from '@components/Error';
 import styles from '@styles/pages/perfil.module.scss';
 import api from 'src/services/api';
 import { GetServerSideProps } from 'next';
-import Cookies from 'js-cookie';
 import {parseCookies} from 'nookies'
-
-function getInitialProps() {
-
-}
 
 export default function Perfil({ user, userType }) {
   if (!user) {
@@ -21,48 +16,44 @@ export default function Perfil({ user, userType }) {
   }
 
   return (
-    <>
-      <div className={styles.container}>
-        <Header text={user.username} />
-        <div className={styles.content}>
-          <div className={styles.img}></div>
+    <div className={styles.container}>
+      <Header text={user.username} />
+      <div className={styles.content}>
+        <div className={styles.img}></div>
 
-          <div className={styles.text}>
-            <h1>{user.name}</h1>
-            <span>@{user.username}</span>
-            <p>{user.bio || 'Sem descrição.'}</p>
-          </div>
-
-          <div className={styles.statistics}>
-            <div className={styles.stat}>
-              <span className={styles.number}>200</span>
-              <span className={styles.text}>patrocinadores</span>
-            </div>
-            <div className={styles.stat}>
-              <span className={styles.number}>15</span>
-              <span className={styles.text}>patrocinando</span>
-            </div>
-          </div>
-
-          { userType === "shop-me" ? ( 
-            <>
-              <Button url="/perfil/editar">Editar</Button>
-              <Button>Compartilhar</Button>
-            </>
-          ) : userType === "me" ? (
-            <Button>Editar</Button> 
-          ) : userType === "shop" ? ( 
-            <Button>Copatrocinando</Button> 
-          ) : userType === "user" ? (
-            <Button>Patrocinar</Button> 
-          ) : 
-            <></>
-          }
-
+        <div className={styles.text}>
+          <h1>{user.name}</h1>
+          <span>@{user.username}</span>
+          <p>{user.bio || 'Sem descrição.'}</p>
         </div>
-        <Menu page="profile" />
+
+        <div className={styles.statistics}>
+          <div className={styles.stat}>
+            <span className={styles.number}>200</span>
+            <span className={styles.text}>patrocinadores</span>
+          </div>
+          <div className={styles.stat}>
+            <span className={styles.number}>15</span>
+            <span className={styles.text}>patrocinando</span>
+          </div>
+        </div>
+
+        { userType === "shop-me" ? ( 
+          <>
+            <Button url="/perfil/editar">Editar</Button>
+            <Button>Compartilhar</Button>
+          </>
+        ) : userType === "me" ? (
+          <Button>Editar</Button> 
+        ) : userType === "shop" ? ( 
+          <Button>Copatrocinando</Button> 
+        ) : userType === "user" && (
+          <Button>Patrocinar</Button> 
+        ) }
+
       </div>
-    </>
+      <Menu page="profile" />
+    </div>
   )
 }
 
@@ -72,43 +63,41 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const { username } = context.query
 
-  let data;
-
   try {
-    data = await api.get(`/profile/${username}`, {
+    const {data} = await api.get(`/profile/${username}`, {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     });
+
+    let userType = ""
+
+    if (user.username === username) {
+      if (user.roles === "shop") {
+        userType = "shop-me"
+      } else {
+        userType = "me"
+      }
+    } else {
+      if (data.roles === "shop") {
+        userType = "shop"
+      } else {
+        userType = "user"
+      }
+    }
+
+    return {
+      props: {
+        user: data,
+        userType,
+      }
+    }
   } catch (err) {
     return {
       props: {
         user: null,
         userType: null,
       }
-    }
-  }
-
-  let userType = ""
-
-  if (user.username === username) {
-    if (user.roles === "shop") {
-      userType = "shop-me"
-    } else {
-      userType = "me"
-    }
-  } else {
-    if (data.data.roles === "shop") {
-      userType = "shop"
-    } else {
-      userType = "user"
-    }
-  }
-
-  return {
-    props: {
-      user: data.data,
-      userType,
     }
   }
 }
