20b114e8f4343e4336ca9c670d6e9fd482d5a180
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-09 22:30:17 -0300

    Refactor: Delete the current page

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -11,33 +11,31 @@ type MenuProps = {
 const Menu: React.FC<MenuProps> = ({ page }) => {
   const {user} = useAuth()
 
-  const [currentPage, setCurrentPage] = useState(page);
-
   return (
     user ? (
       <div className={styles.menu}>
-      <Link href="/dashboard">
-        <a>
-          <button>
-            <img src={currentPage === 'sponsor' ? "/icons/sponsorship_black.png" : "/icons/sponsorship_gray.png"} alt="Patrocínio" />
-            <span className={currentPage === 'sponsor' ? styles.selected : null}>Patrocínios</span>
-          </button>
-        </a>
-      </Link>
-      <Link href="/patrocinar">
-        <a>
-          <button><img src="/icons/plus_gray.png" alt="Ícone de Adicionar" /></button>
-        </a>
-      </Link>
-      <Link href={`/${user.username}`}>
-        <a>
-          <button>
-            <img src={currentPage === 'profile' ? "/icons/profile_black.png" : "/icons/profile_gray.png"} alt="Perfil" />
-            <span className={currentPage === 'profile' ? styles.selected : null}>Perfil</span>
-          </button>
-        </a>
-      </Link>
-    </div>
+        <Link href="/dashboard">
+          <a>
+            <button>
+              <img src={page === 'sponsor' ? "/icons/sponsorship_black.png" : "/icons/sponsorship_gray.png"} alt="Patrocínio" />
+              <span className={page === 'sponsor' ? styles.selected : null}>Patrocínios</span>
+            </button>
+          </a>
+        </Link>
+        <Link href="/patrocinar">
+          <a>
+            <button><img src="/icons/plus_gray.png" alt="Ícone de Adicionar" /></button>
+          </a>
+        </Link>
+        <Link href={`/${user.username}`}>
+          <a>
+            <button>
+              <img src={page === 'profile' ? "/icons/profile_black.png" : "/icons/profile_gray.png"} alt="Perfil" />
+              <span className={page === 'profile' ? styles.selected : null}>Perfil</span>
+            </button>
+          </a>
+        </Link>
+      </div>
     ) : <p>Carregando...</p>
   );
 };
