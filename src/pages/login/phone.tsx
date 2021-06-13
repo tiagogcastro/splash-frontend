2383a1f20e733a35ef6d8e1ba31ca9a10c1f2775
FireShark688 <gugacalb@gmail.com>
2021-06-12 21:03:07 -0300

    feet: Added password input

--- src/pages/login/phone.tsx
+++ src/pages/login/phone.tsx
@@ -10,12 +10,14 @@ export default function LoginPhone() {
   const router = useRouter()
 
   const [userPhone, setUserPhone] = useState('')
+  const [password, setPassword] = useState('')
   
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
     
     await api.post(`/sessions/sms`, {
       phone_number: userPhone,
+      password: password
     })
     
     router.push('/dashboard')
@@ -32,6 +34,9 @@ export default function LoginPhone() {
             <h4>BR +55</h4>
             <hr />
             <input type="tel" placeholder="Numero do telefone" value={userPhone} onChange={(e) => setUserPhone(e.target.value)} />
+          </div>
+          <div className={styles.passwordInput}>
+            <input type="tel" placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
 
           <Button type="submit">Entrar</Button>
