15c896b2727da57ceea55933c133aa7e1f56b202
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-13 19:17:51 -0300

    Feature: Add edit Email

--- src/pages/perfil/editar/email.tsx
+++ src/pages/perfil/editar/email.tsx
@@ -8,20 +8,26 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import api from 'src/services/api';
 
-export default function Email({ email }) {
+export default function Email({ email, token }) {
   const {saveOnCookies} = useAuth()
   const router = useRouter()
 
   const [newEmail, setNewEmail] = useState(email)
+  const [messageVisible, setMessageVisible] = useState(false)
 
   async function handleEditProfile() {
-    const response = await api.put('/profile', {
-      email: newEmail
-    })
-
-    saveOnCookies({ user: response.data })
-
-    router.back()
+    try {
+      const response = await api.put('/profile', {
+        email: newEmail,
+        token
+      })
+  
+      saveOnCookies({ user: response.data })
+  
+      router.back()
+    } catch {
+      setMessageVisible(true)
+    }
   }
 
   return (
@@ -31,8 +37,11 @@ export default function Email({ email }) {
         <div className={styles.content}>
           <div className={styles.field}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" placeholder="Insira seu e-mail..."/>
+            <input type="email" name="email" placeholder="Insira seu e-mail..." value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+            { messageVisible && <p>Um E-mail foi enviado para {newEmail}, confirme ele em até 12 horas.</p> }
           </div>
+
+
           <div className={styles.buttonConfirmation}>
             <Button onClick={handleEditProfile}>Confirmar</Button>
           </div>
@@ -44,10 +53,12 @@ export default function Email({ email }) {
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const email = query.email
+  const token = query.token
 
   return {
     props: {
-      email
+      email,
+      token
     }
   }
 }
