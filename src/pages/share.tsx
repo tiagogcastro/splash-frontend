9769e3db8a9916411bde0bec32a19f49f2891037
Eduardo Bravo <eduardobravopires@gmail.com>
2021-06-15 20:17:11 -0300

    Fix: Format price on /share

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -3,24 +3,22 @@ import Link from 'next/link'
 
 import styles from '@styles/pages/share.module.scss'
 import { useRouter } from 'next/router';
-<<<<<<< HEAD
 import { useEffect, useState } from 'react';
 import api from 'src/services/api';
 import Menu from '@components/Menu';
-import { useAuth } from 'src/hooks/useAuth';
+import { withSSRAuth } from 'src/utils/withSSRAuth';
+import { GetServerSideProps } from 'next';
+import { formatPrice } from 'src/utils/formatPrice';
+import { parseCookies } from 'nookies';
 
 interface Sponsorship {
   amount: number;
   allow_withdrawal: boolean;
   sponsorship_code: string;
 }
-=======
-import { withSSRAuth } from 'src/utils/withSSRAuth';
-import { GetServerSideProps } from 'next';
->>>>>>> 4c6da2a242aabed37b36f03e7f483236d178c429
 
-export default function Share() {
-  const { user } = useAuth();
+
+export default function Share({ user }) {
   const [sponsorship, setSponsorship] = useState<Sponsorship>();
   console.log(sponsorship);
 
@@ -53,7 +51,7 @@ export default function Share() {
               ? 'Valor permitido para saque' 
               : 'Valor não permitido para saque'}
             </span>
-            <span>Patrocínio de {sponsorship.amount}</span>
+            <span>Patrocínio de {formatPrice(sponsorship.amount)}</span>
             <Link href="/">
               <a>lavimco.com</a>
             </Link>
@@ -67,7 +65,11 @@ export default function Share() {
 }
 
 export const getServerSideProps: GetServerSideProps = withSSRAuth(async (ctx) => {
+  const user = JSON.parse(parseCookies(ctx)["%40Lavimco%3Auser"])
+  
   return {
-    props: {}
+    props: {
+      user
+    }
   }
 })
