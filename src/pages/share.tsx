6e9908fabdc48e70b3b2ccbaa71480716eaaed78
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-15 17:44:03 -0300

    feat: Added api before create sponsorship

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -3,30 +3,59 @@ import Link from 'next/link'
 
 import styles from '@styles/pages/share.module.scss'
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import api from 'src/services/api';
+import Menu from '@components/Menu';
+import { useAuth } from 'src/hooks/useAuth';
+
+interface Sponsorship {
+  amount: number;
+  allow_withdrawal: boolean;
+  sponsorship_code: string;
+}
 
 export default function Share() {
+  const { user } = useAuth();
+  const [sponsorship, setSponsorship] = useState<Sponsorship>();
+  console.log(sponsorship);
 
   const router = useRouter()
-  const query = router.query;
+  const value = router.query.value;
 
-  const cod = query.cod as string;
+  useEffect(() => {
+    api.post('/sponsorships/sponsorship-code', {
+      allow_withdrawal_balance: user.role === 'shop',
+      amount: value
+    }).then(response => {
+      setSponsorship(response.data);
+    });
+  }, [value]);
 
   return (
     <>
       <div className={styles.container}>
-        <Header text={"Nome da loja"} />
+        <Header text={user.name} />
+        {!sponsorship ? (<p>Carregando...</p>) : 
         <div className={styles.content}>
           <div className={styles.img}>
             <img src="/qrcode.png" alt="" />
           </div>
 
           <div className={styles.text}>
-            <h2>{cod}</h2>
+            <h2>{sponsorship.sponsorship_code}</h2>
+            <span>
+              {sponsorship.allow_withdrawal 
+              ? 'Valor permitido para saque' 
+              : 'Valor não permitido para saque'}
+            </span>
+            <span>Patrocínio de {sponsorship.amount}</span>
             <Link href="/">
               <a>lavimco.com</a>
             </Link>
           </div>
         </div>
+        }
+        <Menu page="newSponsor" />
       </div>
     </>
   )
