105dd8d85a682ba3846877a48629c68999d9e751
Tiago Gonçalves <tiaguin180@gmail.com>
2021-06-15 13:34:28 -0300

    feat: Added api to sponsored user

--- src/pages/patrocinar/valor.tsx
+++ src/pages/patrocinar/valor.tsx
@@ -1,61 +1,83 @@
 import styles from '@styles/pages/patrocinar/valor.module.scss'
 import Header from '@components/Header'
 import Button from '@components/Button'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
+import { useRouter } from 'next/router';
+import api from 'src/services/api';
+import yup from 'yup';
 
 export default function PatrocinarValor() {
+  const route = useRouter();
+  const [value, setValue] = useState(1);
 
-  function setInputValue (e) {
-
+  function setInputValue(e) {
     let v = parseInt(e.target.value);
-
     if (v > 0 || v) {
-
-      
       if (v > 500) {
-
         setValue(value)
       } else {
-
         setValue(v)
-
       }
       
-    }  else {
+    } else {
       setValue(0)
     }
   }
+  // Falta fazer funcionar o value pra enviar o valor vindo do input
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    
+    try {
+      const schema = yup.object().shape({
+        amount: yup.number().required("Valor obrigatório")
+      });
 
-  const [value, setValue] = useState(1);
+      api.post('/sponsorships', {
+        user_recipient_id: route.query.user_id || null,
+        allow_withdrawal_balance: true,
+        amount: value
+      });
+
+      const data = {
+        amount: value
+      }
+      
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      
+      route.push('/dashboard')
+    } catch(e) {}
+  }
 
   return (
     <div className={styles.container}>
-        <Header text="Enviar patrocínio"/>
-        <div className={styles.content}>
-            <div className={styles.innerContent}>
-                <div className={styles.value}>
-                    <span>R$</span>
-                    <div className={styles.input}>
-                      <input 
-                        onChange={(e) => {
-                          setInputValue(e)
-                        }} 
-                        value={value} 
-                        type="text"
-                        pattern="[0-9]"
-                      />
-                      <div className={styles.alert}>
-                        * valor máximo de R$ 500
-                      </div>
-                    </div>
-                    
-                </div>
+      <Header text="Enviar patrocínio" />
+      <form className={styles.content} onSubmit={(event) => handleSubmit(event)}>
+        <div className={styles.innerContent}>
+          <div className={styles.value}>
+            <span>R$</span>
+            <div className={styles.input}>
+              <input
+                onChange={(e) => {
+                  setInputValue(e)
+                }}
+                value={value}
+                type="number"
+                pattern="[0-9]"
+              />
+              <div className={styles.alert}>
+                * valor máximo de R$ 500
+              </div>
             </div>
-            
-            <div className={styles.buttonConfirmation}>
-              <Button url="/dashboard">Enviar</Button>
-            </div>
+
+          </div>
         </div>
+
+        <div className={styles.buttonConfirmation}>
+          <Button type="submit">Enviar</Button>
+        </div>
+      </form>
     </div>
   )
 }
