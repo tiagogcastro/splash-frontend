3cc1352e24326fc471e0af6d2b662c80fdd29d3a
x14102004x <marcosproenca144@gmail.com>
2021-06-16 13:24:34 -0300

    feat: The company information footer was added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,14 @@ export default function Login() {
           <a>Cadastrar-se</a>
         </Link>
       </div>
+      <footer className={styles.footer}>
+        <strong>Lavimco Tecnologia Ltda</strong> 
+        <p>
+        Rua Sader Macul, nº 96, <strong>São Paulo/SP</strong> - CEP: 04543-907 | CNPJ: 35.576.012/0001-43
+        </p>
+      
+
+      </footer>
     </>
   )
 }
