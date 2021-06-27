ea41173b49ab53a28d8626f68fa1b0a92e9b7f64
Marcos Proença <marcosproenca144@gmail.com>
2021-06-27 20:34:51 -0300

    fix: The creation of multiple sponsorship codes after the reload bug was fixed

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,64 +1,71 @@
 import Header from '@components/Header';
-import Link from 'next/link'
-import styles from '@styles/pages/share.module.scss'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import api from 'src/services/api';
+import Loading from '@components/Loading';
 import Menu from '@components/Menu';
-import { formatPrice } from 'src/utils/formatPrice';
-import { parseCookies } from 'nookies';
-import { useAuth } from 'src/hooks/useAuth';
-import { withSSRAuth } from 'src/utils/withSSRAuth';
+import styles from '@styles/pages/share.module.scss';
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import { parseCookies } from 'nookies';
+import { useEffect, useMemo, useState } from 'react';
+import { useSponsorship } from 'src/hooks/useSponsorship';
+import { formatPrice } from 'src/utils/formatPrice';
+import { withSSRAuth } from 'src/utils/withSSRAuth';
 
-interface Sponsorship {
+interface ISponsorship {
   amount: number;
   allow_withdrawal: boolean;
   sponsorship_code: string;
 }
-
 export default function Share({ user }) {
-  const [sponsorship, setSponsorship] = useState<Sponsorship>();
-
-  const router = useRouter()
-  const value = router.query.value;
+  const { sponsorship: sponsorshipWithoutSSR } = useSponsorship();
+  const [sponsorship, setSponsorship] = useState<ISponsorship>({} as ISponsorship);
 
   useEffect(() => {
-    api.post('/sponsorships/sponsorship-code', {
-      allow_withdrawal_balance: user.role === 'shop',
-      amount: value
-    }).then(response => {
-      setSponsorship(response.data);
-    });
-  }, []);
+    setSponsorship(sponsorshipWithoutSSR)
+  }, [sponsorshipWithoutSSR])
+
+
+  const checkedIfTheSponsorshipObjectIsEmpty = useMemo(() => {
+    if(Object.keys(sponsorship).length === 0)
+      return true
+
+    return false
+  }, [sponsorship])
 
   return (
     <>
       <div className={styles.container}>
-        {!sponsorship || !user.name ? (<p>Carregando...</p>) : (
-        <>
+
         <Header text={user.name} />
         <div className={styles.content}>
-          <div className={styles.img}>
-          <img src={`https://api.lavimco.com/users/qrcode?sponsorship_code=${sponsorship.sponsorship_code}`} alt="Imagem do QrCode" />
-          </div>
 
-          <div className={styles.text}>
-            <h2>{sponsorship.sponsorship_code}</h2>
-            <span>
-              {sponsorship.allow_withdrawal 
-              ? 'Valor permitido para saque' 
-              : 'Valor não permitido para saque'}
-            </span>
-            <span>Patrocínio de {formatPrice(sponsorship.amount)}</span>
-            <Link href="/">
-              <a>lavimco.com</a>
-            </Link>
+          {
+            checkedIfTheSponsorshipObjectIsEmpty
+            ?
+            <div style={{overflow: 'hidden', marginTop: '30vh'}}>
+              <Loading size={30} />
+            </div>
+            :
+            <>
+              <div className={styles.img}>
+                <img src={`https://api.lavimco.com/users/qrcode?sponsorship_code=${sponsorship.sponsorship_code}`} alt="Imagem do QrCode" />
+              </div>
+
+              <div className={styles.text}>
+                <h2>{sponsorship.sponsorship_code}</h2>
+                <span>
+                  {sponsorship.allow_withdrawal
+                  ? 'Valor permitido para saque'
+                  : 'Valor não permitido para saque'}
+                </span>
+                <span>Patrocínio de {formatPrice(sponsorship.amount)}</span>
+                <Link href="/login">
+                  lavimco.com
+                </Link>
+              </div>
+            </>
+          }
           </div>
-        </div>
-        </>
-        )}
-        <Menu page="newSponsor" />
+          <Menu page="newSponsor" />
       </div>
     </>
   )
@@ -66,7 +73,7 @@ export default function Share({ user }) {
 
 export const getServerSideProps: GetServerSideProps = withSSRAuth(async (ctx) => {
   const user = JSON.parse(parseCookies(ctx)["%40Lavimco%3Auser"])
-  
+
   return {
     props: {
       user
